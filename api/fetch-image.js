bff15c46f69477fd392e05380ad8a670ea0b5bb2
adamwayne <adam@adamwayne.com>
2025-11-19 13:05:29 -0800

    Update fetch-image.js

--- api/fetch-image.js
+++ api/fetch-image.js
@@ -8,16 +8,15 @@ export default function handler(req, res) {
     return res.status(400).send("Missing id");
   }
 
-  const filePath = path.join("/tmp", `${id}.png`);
+  const file = path.join("/tmp", `${id}.png`);
 
-  if (!fs.existsSync(filePath)) {
+  if (!fs.existsSync(file)) {
     return res.status(404).send("Image not found");
   }
-
-  const fileStream = fs.createReadStream(filePath);
 
   res.setHeader("Content-Type", "image/png");
   res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
 
-  fileStream.pipe(res);
+  const stream = fs.createReadStream(file);
+  stream.pipe(res);
 }
